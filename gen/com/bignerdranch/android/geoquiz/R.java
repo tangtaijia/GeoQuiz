f75d677ec3a7c30bc5864d49d843c93ccdbecef6
taijia <tangtaijia@qq.com>
2014-11-30 21:57:09 +0800

    chapter2: next question array

--- gen/com/bignerdranch/android/geoquiz/R.java
+++ gen/com/bignerdranch/android/geoquiz/R.java
@@ -22,11 +22,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int arrow_left=0x7f020000;
+        public static final int arrow_right=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int false_button=0x7f080002;
-        public static final int menu_settings=0x7f080003;
+        public static final int menu_settings=0x7f080004;
+        public static final int next_button=0x7f080003;
         public static final int question_text_view=0x7f080000;
         public static final int true_button=0x7f080001;
     }
@@ -38,12 +41,18 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int correct_toast=0x7f050004;
-        public static final int false_button=0x7f050003;
-        public static final int incorrect_toast=0x7f050005;
-        public static final int menu_settings=0x7f050006;
-        public static final int question_text_view=0x7f050001;
-        public static final int true_button=0x7f050002;
+        public static final int correct_toast=0x7f050005;
+        public static final int false_button=0x7f050002;
+        public static final int incorrect_toast=0x7f050006;
+        public static final int menu_settings=0x7f050007;
+        public static final int next_button=0x7f050003;
+        public static final int prev_button=0x7f050004;
+        public static final int question_africa=0x7f05000a;
+        public static final int question_americas=0x7f05000b;
+        public static final int question_asia=0x7f05000c;
+        public static final int question_mideast=0x7f050009;
+        public static final int question_oceans=0x7f050008;
+        public static final int true_button=0x7f050001;
     }
     public static final class style {
         /** 
